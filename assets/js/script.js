4bfdac2810b1e380ac03fc9df7668cbfe4308768
Maria Sargent <maria.marshall.83@gmail.com>
2021-01-07 17:51:56 -0600

    Added question transition and answer buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,20 @@
 var timeEl = document.getElementById("time");
 var mainEl = document.querySelector(".wrapper");
 var startQuiz = document.getElementById("begin");
-var qestionDisplay = document.getElementById("Content");
+var qestionDisplay = document.getElementById("content");
 
-// var buttonOne = document.createElement("bOne");
-// var buttonTwo = document.createElement("bTwo");
-// var buttonThree = document.createElement("bThree");
-// var buttonFour = document.createElement("bFour");
-// var buttonFive = document.createElement("bFive");
-
-//Q&A array
-var questions = [
-  qOne = "What color is the sky?",
-  qTwo = "What color is the laptop?",
-  qThree = "What color is the ceiling?",
-  qFour = "What color is the screen background?",
-  qFive = "What color is the banana?"
-]
-
-var answers = [
-  aOne = "blue", "grey", "white", "black", "yellow",
-  aTwo = "blue", "grey", "white", "black", "yellow",
-  aThree = "blue", "grey", "white", "black", "yellow",
-  aFour = "blue", "grey", "white", "black", "yellow",
-  aFive = "blue", "grey", "white", "black", "yellow"
-]
-
-var firstQ = document.createElement("h2"); //need to create the buttons through JS and connect the content of the Q in container and A in boxes.
-var makeString = JSON.stringify(questions[qOne]);
-console.log(makeString);
-firstQ.textContent = makeString;
-document.body.appendChild(firstQ);
-console.log(firstQ);
-// questionDisplay.children[0].textContent = questions.qOne;
-// questionDisplay.children[1].children[0].textContent = "test";
 
 
 //click on Start Quiz button
 startQuiz.addEventListener("click", function () {
   mainEl.textContent = "";
-  // quizTimer();
-  // quizEvent();
+  quizTimer();
+  quizEvent();
 })
 
 //start timer for quiz
-function quizTimer() {
+function quizTimer(e) {
+  // e.preventDefault();
   var timeLeft = 10;
   var timeInterval = setInterval(function () {
     timeEl.textContent = "Your Timer: " + timeLeft + " seconds";
@@ -54,8 +24,26 @@ function quizTimer() {
       timeEl.textContent = "You are out of time!";
       // finalScore(); //initiate a function to show results and enter initials to record score.
     }
-  }, 500);
+  }, 1000);
 }
+
+
+function quizEvent() {
+  // questionDisplay.setAttribute("style", "visibility: visible;");//NOT WORKING
+  // questionDisplay.setAttribute("style", "visibility: visible");
+document.getElementById("content").style.visibility = "visible";
+}
+
+
+// var firstQ = document.createElement("h2"); //need to create the buttons through JS and connect the content of the Q in container and A in boxes.
+// var makeString = JSON.stringify(questions[qOne]);
+// console.log(makeString);
+// firstQ.textContent = makeString;
+// document.body.appendChild(firstQ);
+// console.log(firstQ);
+// questionDisplay.children[0].textContent = questions.qOne;
+// questionDisplay.children[1].children[0].textContent = "test";
+
 
 // i=0
 
